0ee4a0289b7b1998b901ab6b7d0420e6f82d3b0a
Songül Çelik <songulcelik3537@gmail.com>
2024-02-01 22:26:53 +0300

    v1

--- src/components/dashboard/manager/manager-list.jsx
+++ src/components/dashboard/manager/manager-list.jsx
@@ -1,22 +1,22 @@
 import DataTable, { Column } from "@/components/common/form-fields/data-table";
 import Link from "next/link";
 import React from "react";
-import AdminToolbar from "./admin-toolbar";
+import ManagerToolbar from "./manager-toolbar";
 
 
-const AdminList = ({ data }) => {
+const ManagerList = ({ data }) => {
     const { content, totalPages, number, size } = data;
     const handleToolbar = (row) => {
-        console.log(row);
-        return <AdminToolbar row={row} />;
+       
+        return <ManagerToolbar row={row} />;
     };
     return (
         <div className="container">
-            <Link href="/dashboard/admin/new" className="btn btn-primary mb-3">
+            <Link href="/dashboard/manager/new" className="btn btn-primary mb-3">
                 New
             </Link>
             <DataTable
-                title="Admin List"
+                title="Manager List"
                 dataSource={content}
                 dataKey="id"
                 pagination={true}
@@ -33,4 +33,4 @@ const AdminList = ({ data }) => {
         </div>
     );
 };
-export default AdminList;
+export default ManagerList;
