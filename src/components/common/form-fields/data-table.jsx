d6b746fab742b7b32b676f4c5bb826a2d7906c16
Songül Çelik <songulcelik3537@gmail.com>
2024-02-06 23:01:05 +0300

    v2

--- src/components/common/form-fields/data-table.jsx
+++ src/components/common/form-fields/data-table.jsx
@@ -1,244 +1,300 @@
 "use client";
 import Link from "next/link";
 import React, { useState } from "react";
+
 export const Column = ({ title }) => {
-    return <th scope="col">{title}</th>;
+	return <th scope="col">{title}</th>;
 };
+
 const Row = ({ children, selected, selectionMode, onClick }) => {
-    console.log(selected);
-    return (
-        <tr
-            onClick={onClick}
-            className={selected ? "table-primary" : ""}
-            style={{
-                cursor:
-                    selectionMode && selectionMode !== "none" ? "pointer" : "",
-            }}
-        >
-            {children}
-        </tr>
-    );
+	console.log(selected);
+	return (
+		<tr
+			onClick={onClick}
+			className={selected ? "table-primary" : ""}
+			style={{
+				cursor:
+					selectionMode && selectionMode !== "none" ? "pointer" : "",
+			}}
+		>
+			{children}
+		</tr>
+	);
 };
+
 const Cell = ({ children }) => {
-    return <td>{children}</td>;
+	return <td>{children}</td>;
 };
+
 const NoRecordFound = ({ colLength }) => {
-    return (
-        <tr>
-            <td colSpan={colLength}>No records found</td>
-        </tr>
-    );
+	return (
+		<tr>
+			<td colSpan={colLength}>No records found</td>
+		</tr>
+	);
 };
+
 const FirstPageButton = ({ pageNumber }) => {
-    return (
-        <li className="page-item">
-            <Link
-                className={`page-link ${pageNumber ? "" : "disabled"} `}
-                href="?page=0"
-                aria-label="Previous"
-            >
-                <span aria-hidden="true">&laquo;</span>
-            </Link>
-        </li>
-    );
+	return (
+		<li className="page-item">
+			<Link
+				className={`page-link ${pageNumber ? "" : "disabled"} `}
+				href="?page=0"
+				aria-label="Previous"
+			>
+				<span aria-hidden="true">&laquo;</span>
+			</Link>
+		</li>
+	);
 };
+
 const PageButton = ({ totalPages, pageNumber }) => {
-    return [...new Array(totalPages)].map((_, index) => (
-        <li className="page-item" key={index} aria-current="page">
-            <Link
-                className={`page-link ${
-                    pageNumber === index ? "disabled" : ""
-                }`}
-                href={`?page=${index}`}
-            >
-                {index + 1}
-            </Link>
-        </li>
-    ));
+	return [...new Array(totalPages)].map((_, index) => (
+		<li className="page-item" key={index} aria-current="page">
+			<Link
+				className={`page-link ${
+					pageNumber === index ? "disabled" : ""
+				}`}
+				href={`?page=${index}`}
+			>
+				{index + 1}
+			</Link>
+		</li>
+	));
 };
+
 const LastPageButton = ({ totalPages, pageNumber }) => {
-    return (
-        <li className="page-item">
-            <Link
-                className={`page-link ${
-                    pageNumber >= totalPages - 1 ? "disabled" : ""
-                }`}
-                href={`?page=${totalPages - 1}`}
-                aria-label="Next"
-            >
-                <span aria-hidden="true">&raquo;</span>
-            </Link>
-        </li>
-    );
+	return (
+		<li className="page-item">
+			<Link
+				className={`page-link ${
+					pageNumber >= totalPages - 1 ? "disabled" : ""
+				}`}
+				href={`?page=${totalPages - 1}`}
+				aria-label="Next"
+			>
+				<span aria-hidden="true">&raquo;</span>
+			</Link>
+		</li>
+	);
 };
+
 const Pagination = ({ totalPages, pageNumber, pageSize }) => {
-    if (totalPages <= 1) return null;
-    return (
-        <nav
-            aria-label="Page navigation"
-            className="d-flex justify-content-center "
-        >
-            <ul className="pagination">
-                <FirstPageButton pageNumber={pageNumber} />
-                <PageButton pageNumber={pageNumber} totalPages={totalPages} />
-                <LastPageButton
-                    pageNumber={pageNumber}
-                    totalPages={totalPages}
-                />
-            </ul>
-        </nav>
-    );
+	if (totalPages <= 1) return null;
+
+	return (
+		<nav
+			aria-label="Page navigation"
+			className="d-flex justify-content-center "
+		>
+			<ul className="pagination">
+				<FirstPageButton pageNumber={pageNumber} />
+
+				<PageButton pageNumber={pageNumber} totalPages={totalPages} />
+
+				<LastPageButton
+					pageNumber={pageNumber}
+					totalPages={totalPages}
+				/>
+			</ul>
+		</nav>
+	);
 };
+
 const DataTable = ({
-    title,
-    dataSource,
-    dataKey,
-    pagination,
-    totalPages,
-    pageNumber,
-    pageSize,
-    children,
-    selectionMode, // single | multiple | none
-    selection, // update yapilarinda onceden secili olarak gelmesi istenilen elemanlarin listesi
+	id,
+	title,
+	dataSource,
+	dataKey,
+	pagination,
+	totalPages,
+	pageNumber,
+	pageSize,
+	children,
+	selectionMode, // single | multiple | none
+	selection, // update yapilarinda onceden secili olarak gelmesi istenilen elemanlarin listesi
+	error,
 }) => {
-    const [selectedItems, setSelectedItems] = useState(selection ?? []);
-    if (!dataSource) throw new Error("dataSource attribute is required");
-    if (!Array.isArray(dataSource))
-        throw new Error("dataSource value must be an array");
-    if (!dataKey) throw new Error("dataKey attribute is required");
-    // children korumali bir prop oldugu icin uzerinde degisiklik yapmaya izin vermez
-    // degisiklik yapabilmek icin kopyasini olusturduk
-    const columns = [...children];
-    if (!pageSize) pageSize = 0;
-    if (!pageNumber) pageNumber = 0;
-    if (selectionMode && selectionMode !== "none") {
-        columns.splice(
-            1,
-            0,
-            <Column selectionMode={selectionMode} title="Select" />
-        );
-    }
-    const handleSelectedItems = (id) => {
-        console.log(id)
-        let arr = [...selectedItems];
-        if (!arr.includes(id)) {
-            arr.push(id);
-        } else {
-            arr = arr.filter((item) => item !== id);
-        }
-        setSelectedItems(arr);
-    };
-    //console.log(selectedItems);
-    return (
-        <div className="card">
-            <div className="card-body">
-                <h3 className="card-title">{title}</h3>
-                <div className="table-responsive">
-                    <table className="table table-striped ">
-                        <thead>
-                            <tr>
-                                {columns.map((item) => (
-                                    <Column
-                                        key={item.props.title}
-                                        {...item.props}
-                                    />
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {dataSource.length <= 0 ? (
-                                <NoRecordFound colLength={columns.length} />
-                            ) : null}
-                            {dataSource.map((row, indexRow) => {
-                                //console.log(row);
-                                const selected = selectedItems.includes(
-                                    row[dataKey].toString()
-                                );
-                                return (
-                                    <Row
-                                        key={`row-${row[dataKey]}`}
-                                        selected={selected}
-                                        selectionMode={selectionMode}
-                                        onClick={()=>handleSelectedItems(row[dataKey].toString())}
-                                    >
-                                        {columns.map((column) => {
-                                            const {
-                                                field,
-                                                index,
-                                                title,
-                                                template,
-                                                selectionMode,
-                                            } = column.props;
-                                            let cellData = "";
-                                            if (index) {
-                                                cellData =
-                                                    pageSize * pageNumber +
-                                                    indexRow +
-                                                    1;
-                                            } else if (
-                                                selectionMode &&
-                                                selectionMode !== "none"
-                                            ) {
-                                                cellData =
-                                                    selectionMode ===
-                                                    "single" ? (
-                                                        <input
-                                                            type="radio"
-                                                            className="form-check-input"
-                                                            name="rd"
-                                                            value={row[dataKey]}
-                                                            onChange={(e)=>
-                                                                handleSelectedItems(e.target.value)
-                                                            }
-                                                            checked={selected}
-                                                        />
-                                                    ) : (
-                                                        <input
-                                                            type="checkbox"
-                                                            className="form-check-input"
-                                                            value={row[dataKey]}
-                                                            onChange={(e)=>
-                                                                handleSelectedItems(e.target.value)
-                                                            }
-                                                            checked={selected}
-                                                        />
-                                                    );
-                                            } else if (field) {
-                                                cellData = row[field];
-                                            } else if (template) {
-                                                if (
-                                                    typeof template !==
-                                                    "function"
-                                                ) {
-                                                    throw new Error(
-                                                        "template prop must be a function"
-                                                    );
-                                                }
-                                                cellData = template(row);
-                                            }
-                                            return (
-                                                <Cell
-                                                    key={`col-${row[dataKey]}-${title}`}
-                                                >
-                                                    {cellData}
-                                                </Cell>
-                                            );
-                                        })}
-                                    </Row>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-                {pagination ? (
-                    <Pagination
-                        totalPages={totalPages}
-                        pageNumber={pageNumber}
-                        pageSize={pageSize}
-                    />
-                ) : null}
-            </div>
-        </div>
-    );
+	const [selectedItems, setSelectedItems] = useState(selection ?? []);
+
+	if (!dataSource) throw new Error("dataSource attribute is required");
+	if (!Array.isArray(dataSource))
+		throw new Error("dataSource value must be an array");
+	if (!dataKey) throw new Error("dataKey attribute is required");
+
+	// children korumali bir prop oldugu icin uzerinde degisiklik yapmaya izin vermez
+	// degisiklik yapabilmek icin kopyasini olusturduk
+	const columns = [...children];
+
+	if (!pageSize) pageSize = 0;
+	if (!pageNumber) pageNumber = 0;
+
+	if (selectionMode && selectionMode !== "none") {
+		columns.splice(
+			1,
+			0,
+			<Column selectionMode={selectionMode} title="Select" />
+		);
+	}
+
+	const handleSelectedItems = (id) => {
+		console.log(id);
+		let arr = [...selectedItems];
+
+		if (!arr.includes(id)) {
+			arr.push(id);
+		} else {
+			arr = arr.filter((item) => item !== id);
+		}
+		setSelectedItems(arr);
+	};
+
+	//console.log(selectedItems);
+
+	return (
+		<>
+			<input
+				type="hidden"
+				name={id}
+				value={JSON.stringify(selectedItems)}
+			/>
+			<div className={`card ${error ? "border-danger" : ""}`}>
+				<div className="card-body">
+					<h3 className="card-title">{title}</h3>
+
+					<div className="table-responsive">
+						<table className="table table-striped ">
+							<thead>
+								<tr>
+									{columns.map((item) => (
+										<Column
+											key={item.props.title}
+											{...item.props}
+										/>
+									))}
+								</tr>
+							</thead>
+							<tbody>
+								{dataSource.length <= 0 ? (
+									<NoRecordFound colLength={columns.length} />
+								) : null}
+
+								{dataSource.map((row, indexRow) => {
+									//console.log(row);
+									const selected = selectedItems.includes(
+										row[dataKey].toString()
+									);
+
+									return (
+										<Row
+											key={`row-${row[dataKey]}`}
+											selected={selected}
+											selectionMode={selectionMode}
+											onClick={() =>
+												handleSelectedItems(
+													row[dataKey].toString()
+												)
+											}
+										>
+											{columns.map((column) => {
+												const {
+													field,
+													index,
+													title,
+													template,
+													selectionMode,
+												} = column.props;
+												let cellData = "";
+
+												if (index) {
+													cellData =
+														pageSize * pageNumber +
+														indexRow +
+														1;
+												} else if (
+													selectionMode &&
+													selectionMode !== "none"
+												) {
+													cellData =
+														selectionMode ===
+														"single" ? (
+															<input
+																type="radio"
+																className="form-check-input"
+																name="rd"
+																value={
+																	row[dataKey]
+																}
+																onChange={(e) =>
+																	handleSelectedItems(
+																		e.target
+																			.value
+																	)
+																}
+																checked={
+																	selected
+																}
+															/>
+														) : (
+															<input
+																type="checkbox"
+																className="form-check-input"
+																value={
+																	row[dataKey]
+																}
+																onChange={(e) =>
+																	handleSelectedItems(
+																		e.target
+																			.value
+																	)
+																}
+																checked={
+																	selected
+																}
+															/>
+														);
+												} else if (field) {
+													cellData = row[field];
+												} else if (template) {
+													if (
+														typeof template !==
+														"function"
+													) {
+														throw new Error(
+															"template prop must be a function"
+														);
+													}
+
+													cellData = template(row);
+												}
+
+												return (
+													<Cell
+														key={`col-${row[dataKey]}-${title}`}
+													>
+														{cellData}
+													</Cell>
+												);
+											})}
+										</Row>
+									);
+								})}
+							</tbody>
+						</table>
+					</div>
+					{pagination ? (
+						<Pagination
+							totalPages={totalPages}
+							pageNumber={pageNumber}
+							pageSize={pageSize}
+						/>
+					) : null}
+				</div>
+
+				{error ? <div className="p-3 text-danger">{error}</div> : null}
+			</div>
+		</>
+	);
 };
+
 export default DataTable;
