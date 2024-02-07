db19da3bdedd0fdb30fae7a504d6338edf4fb6c1
Songül Çelik <songulcelik3537@gmail.com>
2024-02-07 22:58:09 +0300

    v2

--- src/app/dashboard/student/[id]/page.jsx
+++ src/app/dashboard/student/[id]/page.jsx
@@ -1,17 +1,18 @@
 import PageHeader from "@/components/common/page-header";
 import Spacer from "@/components/common/spacer";
+import EditStudentForm from "@/components/dashboard/student/edit-student-form";
 import { getStudentById } from "@/services/student-service";
 import { getAllAdvisorTeachers } from "@/services/teacher-service";
 import React from "react";
 const EditStudentPage = async ({ params }) => {
     const dataStudent = (await getStudentById(params.id)).json();
     const dataAdvisors = (await getAllAdvisorTeachers()).json();
-    const [student, advisors] = await Promise.all([dataStudent, dataAdvisors]);
+    const [student, advisorTeachers] = await Promise.all([dataStudent, dataAdvisors]);
     return (
         <>
-            <PageHeader title="Edit Assistant" />
+            <PageHeader title="Edit Student" />
             <Spacer height={50} />
-            <EditAssistantForm student={student.object} advisors={advisors} />
+            <EditStudentForm student={student} advisorTeachers={advisorTeachers} />
             <Spacer />
         </>
     );
